43d1779c14b7c3af8e14d1a124db6b62ef30603d
Juan_Carranza <Juan_Carranza@epam.com>
2023-04-10 16:15:34 -0500

    Add exception to ArrayValidation

--- ArrayValidation.ps1
+++ ArrayValidation.ps1
@@ -1,11 +1,19 @@
- while($array.Length -eq 0)
+$retry = 0
+ while(($array.Length -eq 0) -and ($retry -ne 10))
  {
     $TagList = git tag
-    $array = @($TagList) 
-    Write-Host "array lenght inside loop"
-    Write-Host $array.Length
-    Write-Host "last item array:"
-    Write-Host $array[-1]
-    $NewTag = git tag NewTag
-    Write-Host $NewTag
+    $array = @($TagList)
+    $retry++ 
+    Write-Host "Retry Tags" $retry
+    #Write-Host "array lenght inside loop"
+    #Write-Host $array.Length
+    #Write-Host "last item array:"
+    #Write-Host $array[-1]
+    #$NewTag = git tag NewTag
+    #Write-Host $NewTag
+ }
+
+ if ($retry -eq 10)
+ {
+    Write-Error -Message "Unable to connect with Git. Verify the permission access and network" -ErrorAction Stop
  }
