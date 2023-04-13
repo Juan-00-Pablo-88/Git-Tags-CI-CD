901e36dfca4fdbe8487ab10a74da5042b113a5da
Juan_Carranza <Juan_Carranza@epam.com>
2023-04-13 09:56:40 -0500

    Test Powershell

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,4 +1,5 @@
-Write-Host "Hello World from Powershell Test"  > output.txt
-Write-Host "File generated on path: " + $PWD.Path
+> "$($PWD.Path)\output.txt"
+Write-Host "Hello World from Powershell Test"
+Write-Host "File generated on path: $PWD"
 Write-Host "The elements on that path are: "
 ls | Select-Object Name | ForEach-Object { Write-Host $_.Name }
