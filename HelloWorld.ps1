496bc323c19967c68c1cf7826074d8ce40cfb5bb
Juan_Carranza <Juan_Carranza@epam.com>
2023-04-13 09:46:58 -0500

    Test Powershell

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,2 +1,2 @@
 Write-Host "Hello World from Powershell Test"  > output.txt
-Write-Host "El archivo se generó en la ruta: " + $PWD.Path + "\output.txt"
+Write-Host "El archivo se generó en la ruta: " + $PWD.Path
