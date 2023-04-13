95c3f876b23095c415c273abec99534c801548fe
Juan_Carranza <Juan_Carranza@epam.com>
2023-04-13 09:59:35 -0500

    Test Powershell

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,4 +1,4 @@
-> "$($PWD.Path)\output.txt"
+Out-File -FilePath "$($PWD.Path)\output.txt" -Encoding utf8 -Append
 Write-Host "Hello World from Powershell Test"
 Write-Host "File generated on path: $PWD"
 Write-Host "The elements on that path are: "
